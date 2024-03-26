7aa44e3580f2b9eb7a3088c98058f2f23b26c7eb
unknown <huy.lgia123@gmail.com>
2024-03-26 14:36:14 +0700

    Forest 1

--- THELOOP/Assets/Scripts/AttackController.cs
+++ THELOOP/Assets/Scripts/AttackController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AttackController : MonoBehaviour
 {
-    public int attackDamage = 10;
+    public int attackDamage = 30;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
